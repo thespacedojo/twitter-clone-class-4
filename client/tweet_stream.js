4e5586c05ef1831074a69aa7557c30804c1d12d7
Josh Owens <joshua.owens@gmail.com>
2014-12-13 13:09:09 -0500

    Add geolocationa and cordova support. Fixes #13

--- client/tweet_stream.js
+++ client/tweet_stream.js
@@ -21,7 +21,10 @@ Template.TweetStream.events({
   'submit form': function(event, template) {
     event.preventDefault();
     tweet = template.$('.tweet-text').val();
-    Tweets.insert({text: tweet}, function() {
+    loc = {}
+    if (Session.get('location'))
+      loc = {lat: Session.get('location').coords.latitude, long: Session.get('location').coords.longitude}
+    Tweets.insert({text: tweet, location: loc}, function() {
       template.$('.tweet-text').val(null)
     })
   }
